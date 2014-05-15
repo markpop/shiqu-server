60240e3fc416897e3d682399309e8d3e13b95dfa
jason <jason@weazm.com>
2014-05-15 21:59:40 +0800

    modify one

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
 app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public/app')));
+app.use(express.static(path.join(__dirname, 'public/')));
 
 // development only
 if ('development' == app.get('env')) {
